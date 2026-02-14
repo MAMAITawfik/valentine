79ec2e28a797ae15737909fb3f60cbf8ee8d5a94
tawfik <tawfik.mamai@ecole.ensicaen.fr>
2026-02-14 03:54:11 +0100

    test updated

--- src/components/OrientationGuard.tsx
+++ src/components/OrientationGuard.tsx
@@ -46,11 +46,11 @@ export default function OrientationGuard({
           </svg>
         </motion.div>
         <h2 className="text-3xl font-bold text-white mb-4">
-          Rotate your device
+          Azy SOSO, tourne ton téléphone 😘
         </h2>
         <p className="text-gray-400 text-lg max-w-xs">
-          To be able to play correctly, you need to put your phone in{" "}
-          <b>landscape mode</b>.
+          Si tu restes en mode portrait, tu vas rater la surprise.
+          Mets-toi en paysage, et viens gagner ton cadeau de la HEESSS
         </p>
       </div>
     );
